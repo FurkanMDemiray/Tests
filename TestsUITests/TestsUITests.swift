1cd626b5eb1e3566b2f9deffa7cce0c6c7fb4ac2
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-01-11 02:16:32 +0300

    added add button UIText

--- TestsUITests/TestsUITests.swift
+++ TestsUITests/TestsUITests.swift
@@ -9,33 +9,24 @@ import XCTest
 
 final class TestsUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testToDoUI()throws {
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let addButton = app.navigationBars["Tests.View"].buttons["Item"]
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+
+        let textField = app.alerts["Add ToDo"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Enter ToDo"]/*[[".cells.textFields[\"Enter ToDo\"]",".textFields[\"Enter ToDo\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let okButton = app.alerts["Add ToDo"].scrollViews.otherElements.buttons["OK"]
+        let cell = app.tables/*@START_MENU_TOKEN@*/.staticTexts["Buy milk"]/*[[".cells.staticTexts[\"Buy milk\"]",".staticTexts[\"Buy milk\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+
+        addButton.tap()
+        textField.tap()
+        textField.typeText("Buy milk")
+        okButton.tap()
+
+        XCTAssertTrue(cell.exists)
     }
 }
